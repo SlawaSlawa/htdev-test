d7b799174cd976914d60e0c4ee6a557800221347
SlawaSlawa <slawapocht4@yandex.ru>
2023-02-05 16:50:25 +0400

    added media & toggleSlider for section advantages

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
 'use strict'
+// SLIDER ADVANTAGES
+
+const sliderAdvantages = document.querySelector('.swiper.advantages__slider')
+let mySwiper
+
+function mobileSliderAdvantages() {
+    if (window.innerWidth <= 1019 && sliderAdvantages.dataset.mobile === 'false') {
+        mySwiper = new Swiper(sliderAdvantages, {
+            loop: true,
+            pagination: {
+                el: '.swiper-pagination.swiper-pagination--advantages',
+                clickable: true,
+            },
+            navigation: {
+                nextEl: '.swiper-button-next.swiper-button-next--advantages',
+                prevEl: '.swiper-button-prev.swiper-button-prev--advantages',
+            },
+            breakpoints: {
+                300: {
+                    slidesPerView: 1,
+                    spaceBetween: 25,
+                },
+                699: {
+                    slidesPerView: 2,
+                    spaceBetween: 25,
+                }
+            }
+        })
+        sliderAdvantages.dataset.mobile = true
+    }
+    if (window.innerWidth > 1019) {
+        sliderAdvantages.dataset.mobile = false
+        if (sliderAdvantages.classList.contains('swiper-initialized')) {
+            mySwiper.destroy()
+        }
+    }
+}
+
+mobileSliderAdvantages()
+window.addEventListener('resize', mobileSliderAdvantages)
